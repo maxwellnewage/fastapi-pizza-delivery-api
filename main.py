bf849063481240fd60c816551d5220c7bd653b75
Maximiliano Burgos <maxib2007@gmail.com>
2023-01-09 16:50:58 -0300

    se configuraron las rutas del proyecto

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from fastapi import status
-
+from auth_routes import auth_router
+from order_routes import order_router
 
 app = FastAPI()
 
-
-@app.get('/', status_code=status.HTTP_200_OK)
-async def home():
-    return {"message": "Is working!"}
+app.include_router(auth_router)
+app.include_router(order_router)

341c6a79947075feeb1245ae644d63c7b5f6b527
Maximiliano Burgos <maxib2007@gmail.com>
2023-01-27 09:36:28 -0300

    desacoplo la funcionalidad de JWT para volverla mas global

---
+++ auth_manager.py
@@ -0,0 +1,38 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from fastapi.security import OAuth2PasswordBearer
+from fastapi.exceptions import HTTPException
+from fastapi import status
+
+ALGORITHM = "HS256"
+TOKEN_DURATION_MINUTES = 30
+SECRET_KEY = 'bda3db1e23c619aa4f6ebb4d9398d5535b999a6e364eed6fcef526c4e1ee5cb2'
+
+crypt = CryptContext(schemes=["bcrypt"])
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def get_access_token(username: str):
+    access_token = {
+        "sub": username,
+        "exp": datetime.utcnow() + timedelta(minutes=TOKEN_DURATION_MINUTES)
+    }
+
+    return {
+        'access_token': jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM),
+        "token_type": "bearer"
+    }
+
+
+def get_payload(token: str):
+    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+    if payload.get("sub") is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    return payload.get("sub")
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,20 +1,11 @@
 from fastapi import APIRouter, status, Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 from database import Session, engine
-from schemas import SignUpModel, LoginModel
+from schemas import SignUpModel
 from models import User
 from fastapi.exceptions import HTTPException
-from jose import jwt
-from passlib.context import CryptContext
-from datetime import datetime, timedelta
+from auth_manager import get_access_token, crypt, oauth2_scheme, get_payload
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_DURATION = 10
-SECRET_KEY = 'bda3db1e23c619aa4f6ebb4d9398d5535b999a6e364eed6fcef526c4e1ee5cb2'
-
-crypt = CryptContext(schemes=["bcrypt"])
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 auth_router = APIRouter(
     prefix='/auth',
@@ -26,16 +17,9 @@ session = Session(bind=engine)
 
 @auth_router.get('/')
 async def hello(token: str = Depends(oauth2_scheme)):
-    payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    user_payload = get_payload(token)
 
-    if payload.get("sub") is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    return {"message": "Hello auth"}
+    return {"message": f"Hello {user_payload}!, you are logged in."}
 
 
 @auth_router.post('/signup', response_model=SignUpModel, status_code=status.HTTP_201_CREATED)
@@ -67,15 +51,7 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     user = session.query(User).filter(User.username == form.username).first()
 
     if user and crypt.verify(form.password, user.password):
-        access_token = {
-            "sub": user.username,
-            "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
-        }
-
-        return {
-            'access_token': jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM),
-            "token_type": "bearer"
-        }
+        return get_access_token(user.username)
 
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST,

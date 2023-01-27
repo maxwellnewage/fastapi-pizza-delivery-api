c3e95c0d4840417746470c0490bf81ef50f19932
Maximiliano Burgos <maxib2007@gmail.com>
2023-01-27 10:50:56 -0300

    implementado creacion de orden de una pizza

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -1,11 +1,39 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from models import User, Order
+from schemas import OrderModel
+from auth_manager import oauth2_scheme, get_payload
+from database import Session, engine
+from fastapi.encoders import jsonable_encoder
 
 order_router = APIRouter(
-    prefix='/order',
+    prefix='/orders',
     tags=['orders']
 )
 
+session = Session(bind=engine)
 
-@order_router.get('/')
-async def hello():
-    return {"message": "Hello order"}
+
+@order_router.post('/order', status_code=status.HTTP_201_CREATED)
+async def place_an_order(order: OrderModel, token: str = Depends(oauth2_scheme)):
+    user_payload = get_payload(token)
+
+    user = session.query(User).filter(User.username == user_payload).first()
+
+    new_order = Order(
+        pizza_size=order.pizza_size,
+        quantity=order.quantity,
+    )
+
+    new_order.user = user
+
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "id": new_order.id,
+        "pizza_size": new_order.pizza_size,
+        "quantity": new_order.quantity,
+        "order_status": new_order.order_status
+    }
+
+    return jsonable_encoder(response)

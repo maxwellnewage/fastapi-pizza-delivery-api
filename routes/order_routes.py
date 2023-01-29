11ea8e7dbf7d461c0bc0bfb04f4a0f498255c070
Maximiliano Burgos <maxib2007@gmail.com>
2023-01-29 18:58:09 -0300

    implementado endpoint para listar todas las ordenes por user admin

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -4,6 +4,7 @@ from schemas import OrderModel
 from auth_manager import oauth2_scheme, get_payload
 from database import Session, engine
 from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import HTTPException
 
 order_router = APIRouter(
     prefix='/orders',
@@ -11,6 +12,19 @@ order_router = APIRouter(
 )
 
 session = Session(bind=engine)
+
+
+@order_router.get('/')
+async def get_all_orders(token: str = Depends(oauth2_scheme)):
+    user_payload = get_payload(token)
+
+    user = session.query(User).filter(User.username == user_payload).first()
+
+    if user.is_staff:
+        orders = session.query(Order).all()
+        return orders
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not a superuser")
 
 
 @order_router.post('/order', status_code=status.HTTP_201_CREATED)

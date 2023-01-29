b25e03a3c78ee69101605812cbd1a039befb576d
Maximiliano Burgos <maxib2007@gmail.com>
2023-01-29 19:16:13 -0300

    implementado endpoint para mostrar orden por id

--- routes/order_routes.py
+++ routes/order_routes.py
@@ -14,24 +14,30 @@ order_router = APIRouter(
 session = Session(bind=engine)
 
 
+def get_user(token: str):
+    user_payload = get_payload(token)
+    return session.query(User).filter(User.username == user_payload).first()
+
+
+def is_admin(user):
+    if user.is_staff:
+        return True
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not a superuser")
+
+
 @order_router.get('/')
 async def get_all_orders(token: str = Depends(oauth2_scheme)):
-    user_payload = get_payload(token)
+    user = get_user(token)
 
-    user = session.query(User).filter(User.username == user_payload).first()
-
-    if user.is_staff:
+    if is_admin(user):
         orders = session.query(Order).all()
         return orders
-
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not a superuser")
 
 
 @order_router.post('/order', status_code=status.HTTP_201_CREATED)
 async def place_an_order(order: OrderModel, token: str = Depends(oauth2_scheme)):
-    user_payload = get_payload(token)
-
-    user = session.query(User).filter(User.username == user_payload).first()
+    user = get_user(token)
 
     new_order = Order(
         pizza_size=order.pizza_size,
@@ -51,3 +57,11 @@ async def place_an_order(order: OrderModel, token: str = Depends(oauth2_scheme))
     }
 
     return jsonable_encoder(response)
+
+
+@order_router.get('/{id}')
+async def get_order_by_id(id: int, token: str = Depends(oauth2_scheme)):
+    user = get_user(token)
+
+    if is_admin(user):
+        return session.query(Order).filter(Order.id == id).first()
